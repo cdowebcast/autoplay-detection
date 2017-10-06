f619e14c62812bcb31f90485e73df7edb846c5cd
jermspeaks <jermspeaks@gmail.com>
2017-10-06 12:02:27 -0700

    added rollup to configure package

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/main.min.js',
+      format: 'umd',
+      name: 'autoplayDetector'
+    },
+    sourcemap: 'inline'
+};
